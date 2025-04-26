8d18a3e90665b5372edb7d8ad936a5754ae3f6fc
Timothy Summers <timsummers@mac.com>
2025-04-26 11:20:20 +0200

    commenting for harmony state clarified

--- include/harmony_state.h
+++ include/harmony_state.h
@@ -14,14 +14,19 @@
  * @struct harmony_state
  * @brief Represents the state of a harmonic structure.
  *
- * This struct contains information about the encoded state, chromatic notes,
- * and KPDVE (Key, Pattern, Degree, Voicing, Extension) values. It also includes
+ * This struct contains information about the encoded state (32-bit, completed state), 
+ * chromatic notes (12-bit, encoded right to left as in Hebrew),
+ * and KPDVE (Key, Pattern, Degree, Voicing, Extension) values (16-bit). 
+ * 
+ * the min_index is the closest to the previous analyzed state in KPD (12x7x7) space.
+ * 
+ * It also includes
  * lists for possible KPDVE encodings and their associated distances.
  */
 struct harmony_state {
     int encoded_state; /**< Encoded binary representation of the state (32 bits). */
     int chromatic_notes; /**< Chromatic notes represented as a 12-bit integer (right to left, as in Hebrew). */
-    int kpdve; /**< Encoded KPDVE value (3 bits for each of the first 4 values, 4 bits for the sign). */
+    int kpdve; /**< Encoded KPDVE value (4 bits for key; 3 bits for each of the next 4 values, 3 spare, 1 (leftmost) for invalid state). */
     int dve; /**< Encoded Degree-Voicing-Extension (DVE) value. */
     int ve; /**< Encoded Voicing-Extension (VE) value. */
     int kpdve_list_length; /**< Number of valid KPDVE encodings in the list. */
