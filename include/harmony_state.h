3655f4b80aa2f9aa1dfdc49692a22d7f096bc1f1
Timothy Summers <timsummers@mac.com>
2025-04-26 11:50:50 +0200

    harmony state struct clarified in header file

--- include/harmony_state.h
+++ include/harmony_state.h
@@ -24,12 +24,12 @@
  * lists for possible KPDVE encodings and their associated distances.
  */
 struct harmony_state {
-    int encoded_state; /**< Encoded binary representation of the state (32 bits). */
-    int chromatic_notes; /**< Chromatic notes represented as a 12-bit integer (right to left, as in Hebrew). */
-    int kpdve; /**< Encoded KPDVE value (4 bits for key; 3 bits for each of the next 4 values, 3 spare, 1 (leftmost) for invalid state). */
-    int dve; /**< Encoded Degree-Voicing-Extension (DVE) value. */
-    int ve; /**< Encoded Voicing-Extension (VE) value. */
-    int kpdve_list_length; /**< Number of valid KPDVE encodings in the list. */
+    int encoded_state; /**< Encoded binary representation of the state (32 bits). x---KKKKPPPDDDVVVEEEb-a-g-fe-d-c */
+    int chromatic_notes; /**< Chromatic notes represented as a 12-bit integer (right to left, as in Hebrew). b-a-g-fe-d-c */
+    int kpdve; /**< Encoded KPDVE value (4 bits for key; 3 bits for each of the next 4 values, 3 spare, 1 (leftmost) for invalid state). KKKKPPPDDDVVVEEE*/
+    int dve; /**< Encoded Degree-Voicing-Extension (DVE) value. reduces chord to seven possible notes, independent of key */
+    int ve; /**< Encoded Voicing-Extension (VE) value. reduces chord to least possible value, which defines a 'root' */
+    int kpdve_list_length; /**< Number of valid KPDVE encodings in the list. (up to 84 for memory safety)*/
     int kpdve_min_index; /**< Index of the minimum distance KPDVE encoding. */
     int kpdve_list[84]; /**< List of possible KPDVE encodings (maximum size: 84). */
     int dve_list[84]; /**< List of associated DVE values for each KPDVE encoding. */
